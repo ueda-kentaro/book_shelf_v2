7f8b0ba951235568594194c85bbd36a4cd07f2b8
ueda kentaro <nowallcloud9+ueda@gmail.com>
2016-12-24 08:05:19 +0000

    編集機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,23 @@ class BooksController < ApplicationController
     @book.save
     redirect_to books_path
   end
+  def edit
+    @book= Book.find(params[:id])
+    
+  end
+  def update
+    @book= Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+  
+  
+  
+  
+  
+  
+  
+  
   private
    def book_params
      params.require(:book).permit(:name,:price,:publish,:publish_date)
