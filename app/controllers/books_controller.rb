3f384997f2de31830ca1b33ffca1bfac95ef83fa
ueda kentaro <nowallcloud9+ueda@gmail.com>
2016-12-24 08:38:58 +0000

    rootのルーティング追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
+  before_action :set_book, only:[:show,:edit,:update,:destroy]
+  
   def index
     @books = Book.all
   end
   def show
-    @book = Book.find(params[:id])
   end
   def new
     @book = Book.new
@@ -14,15 +15,15 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
   def edit
-    @book= Book.find(params[:id])
-    
   end
   def update
-    @book= Book.find(params[:id])
     @book.update(book_params)
     redirect_to books_path
   end
-  
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
   
   
   
@@ -33,5 +34,8 @@ class BooksController < ApplicationController
   private
    def book_params
      params.require(:book).permit(:name,:price,:publish,:publish_date)
+   end
+   def set_book
+     @book= Book.find(params[:id])
    end
 end
